c6e1b631eee907fa967777d882e15030baa4f8e4
Jayson-Feguis <jaysonfeguis121714@gmail.com>
2022-11-15 23:01:53 +0800

    initial commit

---
+++ component/client/header/header.php
@@ -0,0 +1,30 @@
+<?php  include('utils/db_config.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>G Verzosa - Salon & Spa</title>
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Montserrat:wght@100;400;700&display=swap" rel="stylesheet">
+     <!-- STYLE -->
+    <link rel="stylesheet" type="text/css" href="./component/client/header/style.css"/>
+</head>
+
+
+<body>
+    <nav class="div-center">
+        <div class="nav-container">
+            <div class="logo"> logo</div>
+            <ul class="nav-items">
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+            </ul>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+include('component/client/header/header.php');
+?>
+
+<section id='section1'><?php echo BASE_URL ?></section>
+    
+<?php 
+include('component/client/footer/footer.php');
+?>
+
+    
