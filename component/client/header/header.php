cd51b47322f927dfbe4839621644f4ba731341f6
Jayson-Feguis <jaysonfeguis121714@gmail.com>
2023-01-01 22:20:25 +0800

    no message

--- component/client/header/header.php
+++ component/client/header/header.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_URI'] == "/") {
+if ($_SERVER['REQUEST_URI'] == "/gverzosa/") {
     include('utils/db_config.php');
     include('utils/routes.php');
 } else {
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_URI'] == "/") {
     <!-- SWIPER JS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <?php
-    if ($_SERVER['REQUEST_URI'] == "/") {
+    if ($_SERVER['REQUEST_URI'] == "/gverzosa/") {
     ?>
         <!-- STYLE -->
         <link rel="stylesheet" type="text/css" href="style/header.css" />
@@ -116,7 +116,7 @@ if ($_SERVER['REQUEST_URI'] == "/") {
             <ul class="nav-items justify-center items-center gap-[28px] pr-[40px] uppercase hidden md:flex">
                 <?php
                 // HOME PAGE
-                if ($_SERVER['REQUEST_URI'] == "/") {
+                if ($_SERVER['REQUEST_URI'] == "/gverzosa/") {
                 ?>
 
                     <li class="active home"> <a href="#home" class=" home  hover:cursor-pointer hover:text-secondary transition-color duration-300">Home</a> </li>
@@ -126,7 +126,7 @@ if ($_SERVER['REQUEST_URI'] == "/") {
                     <button id="btn-book" onclick="openBookModal()" class="bg-secondary text-defaultwhite text-[20px] font-bold py-[5px] px-[10px] rounded-md font-Montserrat transition-all duration-300 hover:bg-tertiary">Book Now</button>
 
                 <?php
-                } else if ($_SERVER['REQUEST_URI'] == "/pages/login.php") {
+                } else if ($_SERVER['REQUEST_URI'] == "/gverzosa/pages/login.php") {
                 ?>
                     <a class="hover:cursor-pointer hover:text-secondary transition-color duration-300" href="../">Home</a>
                     <a class="hover:cursor-pointer hover:text-secondary transition-color duration-300" href="../#about">About Us</a>
@@ -143,7 +143,7 @@ if ($_SERVER['REQUEST_URI'] == "/") {
         <ul class="nav-items justify-center items-center gap-[28px] uppercase flex flex-col md:hidden">
             <?php
             // HOME PAGE
-            if ($_SERVER['REQUEST_URI'] == "/") {
+            if ($_SERVER['REQUEST_URI'] == "/gverzosa/") {
             ?>
                 <li> <a class="home hover:cursor-pointer hover:text-secondary transition-color duration-300" href="#home">Home</a></li>
                 <li> <a class="about hover:cursor-pointer hover:text-secondary transition-color duration-300" href="#about">About Us</a></li>
@@ -152,7 +152,7 @@ if ($_SERVER['REQUEST_URI'] == "/") {
                 <button id="btn-book" onclick="openBookModal()" class="bg-secondary text-defaultwhite text-[20px] font-bold py-[5px] px-[10px] rounded-md font-Montserrat transition-all duration-300 hover:bg-tertiary">Book Now</button>
 
             <?php
-            } else if ($_SERVER['REQUEST_URI'] == "/pages/login.php") {
+            } else if ($_SERVER['REQUEST_URI'] == "/gverzosa/pages/login.php") {
             ?>
                 <a class="hover:cursor-pointer hover:text-secondary transition-color duration-300" href="../">Home</a>
                 <a class="hover:cursor-pointer hover:text-secondary transition-color duration-300" href="../#about">About Us</a>
@@ -170,10 +170,10 @@ if ($_SERVER['REQUEST_URI'] == "/") {
             <div class="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
                 <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                     <form action=<?php
-                                    if ($_SERVER['REQUEST_URI'] == "/") {
+                                    if ($_SERVER['REQUEST_URI'] == "/gverzosa/") {
                                         echo './api/post_book.php';
                                     } else {
-                                        echo '../../../api/post_book.php';
+                                        echo '../../../gverzosa/api/post_book.php';
                                     }
                                     ?> method="post">
                         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
