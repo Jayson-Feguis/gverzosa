145ac9e3c755e61b006fabb6b352ecd7df30dc5d
Jayson-Feguis <jaysonfeguis121714@gmail.com>
2023-01-10 00:05:47 +0800

    fixed error

---
+++ utils/env_config.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USERNAME', 'u586533371_gverzosa');
+    define('DB_PASSWORD', '2k!yp$3Q');
+    define('DB_NAME', 'u586533371_gverzosa'); 
+?>
